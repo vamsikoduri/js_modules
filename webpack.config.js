db1074af6ed19bd9938e1426f3d54050e5178d1f
vamsi krishna <vamc.koduri@gmail.com>
2018-03-09 23:37:47 +0530

    03 handling project assets

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ entry:'./src/index.js',
 output: {
     path:path.resolve(__dirname,'build'),
     filename: 'bundle.js'
+},
+module:{
+    rules:[
+        {
+            use:'babel-loader',
+            test:/\.js$/,
+        }
+    ]
 }
 
 }
